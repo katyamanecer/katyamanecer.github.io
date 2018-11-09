e492679084d40a970397f09a4dd2681c4a282fff
katyamanecer <34971463+katyamanecer@users.noreply.github.com>
2018-11-08 19:40:32 -0600

    Add files via upload

---
+++ Globos birthday.js
@@ -0,0 +1,38 @@
+var today = new Date();
+document.writeln('<scr' + 'ipt type="text/javascript">');
+
+document.writeln('// Floating image script- By Virtual_Max (http://www.geocities.com/siliconvalley/lakes/8620)');
+document.writeln('// Modified by Dynamic Drive for various improvements');
+document.writeln('// Visit Dynamic Drive at http://www.dynamicdrive.com/ for full source code');
+for (var p=0; p<pallonciniMV2.length; p++) {
+document.writeln('var balloonC2' + pallonciniMV2[p] + ';');
+}
+document.writeln('function MVBDay(){');
+for (var p=0; p<pallonciniMV2.length; p++) {
+document.writeln('balloonC2' + pallonciniMV2[p] + '=new Chip("balloonC2' + pallonciniMV2[p] + '",70,150);');
+}
+for (var p=0; p<pallonciniMV2.length; p++) {
+document.writeln('movechip("balloonC2' + pallonciniMV2[p] + '");');
+}
+document.writeln('}');
+document.writeln('if (window.addEventListener)');
+document.writeln('window.addEventListener("load", MVBDay, false)');
+document.writeln('else if (window.attachEvent)');
+document.writeln('window.attachEvent("onload", MVBDay)');
+document.writeln('else if (document.getElementById)');
+document.writeln('window.onload=MVBDay');
+for (var p=0; p<pallonciniMV2.length; p++) {
+document.writeln('var d' + pallonciniMV2[p] + ' = document.createElement("div");');
+document.writeln('d' + pallonciniMV2[p] + '.id="balloonC2' + pallonciniMV2[p] + '";');
+document.writeln('document.body.appendChild(d' + pallonciniMV2[p] + ');');
+document.writeln('document.getElementById("balloonC2' + pallonciniMV2[p] + '").style.position="absolute";');
+document.writeln('document.getElementById("balloonC2' + pallonciniMV2[p] + '").style.top="0";');
+document.writeln('document.getElementById("balloonC2' + pallonciniMV2[p] + '").style.left="-500px";');
+document.writeln('document.getElementById("balloonC2' + pallonciniMV2[p] + '").style.width="90px";');
+document.writeln('document.getElementById("balloonC2' + pallonciniMV2[p] + '").style.height="180px";');
+document.writeln('document.getElementById("balloonC2' + pallonciniMV2[p] + '").style.background="transparent";');
+document.writeln('document.getElementById("balloonC2' + pallonciniMV2[p] + '").style.border="none";');
+document.writeln('document.getElementById("balloonC2' + pallonciniMV2[p] + '").innerHTML= \'<a href="
+https://shaddaikawaii.blogspot.com/2018/11/cumple-de-mi-blog.html" target="_blank"><img title="&copy; shaddaikawaii.blogspot.com" src="https://1.bp.blogspot.com/-O4cRtKRsVi8/W-R96T6k3_I/AAAAAAAAZO4/ki03VNVIs_sWG7XXS6bExyJUT5d-uuErwCLcBGAs/s1600/globo1.png" style="width: 70px; height: 150px; border: none;" /></a>\';');
+}
+document.writeln('</scr' + 'ipt>');
