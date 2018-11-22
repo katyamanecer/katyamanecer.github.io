dcf911bca6e638f7139b610d78a8c48b2bc95a66
katyamanecer <34971463+katyamanecer@users.noreply.github.com>
2018-11-22 02:41:06 -0600

    Add files via upload

---
+++ personalizadoglobos.js
@@ -0,0 +1,70 @@
+<script type="text/javascript">
+// script personalizzato da iolecal.blogspot.it
+// Floating image script- By Virtual_Max (http://www.geocities.com/siliconvalley/lakes/8620)
+// Modified by Dynamic Drive for various improvements
+// Visit Dynamic Drive at http://www.dynamicdrive.com/ for full source code
+var palloncinoC06;
+var palloncinoC12;
+var palloncinoC18;
+var palloncinoC29;
+function pagestartBlogBDay(){
+palloncinoC06=new Chip("palloncinoC06",64,128);
+palloncinoC12=new Chip("palloncinoC12",64,128);
+palloncinoC18=new Chip("palloncinoC18",64,128);
+palloncinoC29=new Chip("palloncinoC29",64,128);
+movechip("palloncinoC06");
+movechip("palloncinoC12");
+movechip("palloncinoC18");
+movechip("palloncinoC29");
+}
+if (window.addEventListener)
+window.addEventListener("load", pagestartBlogBDay, false)
+else if (window.attachEvent)
+window.attachEvent("onload", pagestartBlogBDay)
+else if (document.getElementById)
+window.onload=pagestartBlogBDay
+var d06 = document.createElement("div");
+d06.id="palloncinoC06";
+document.body.appendChild(d06);
+document.getElementById("palloncinoC06").style.position="absolute";
+document.getElementById("palloncinoC06").style.top="0";
+document.getElementById("palloncinoC06").style.left="-500px";
+document.getElementById("palloncinoC06").style.width="90px";
+document.getElementById("palloncinoC06").style.height="180px";
+document.getElementById("palloncinoC06").style.background="transparent";
+document.getElementById("palloncinoC06").style.border="none";
+document.getElementById("palloncinoC06").innerHTML= '<a href="http://iolecal.blogspot.it/2014/03/palloncini-per-il-compleanno-del-blog.html" target="_blank"><img title="&copy; iolecal.blogspot.it" src="https://www.iolecal.it/blog/posts400/post445/10/p_06.png" style="width: 64px; height: 128px; border: none;" /></a>';
+var d12 = document.createElement("div");
+d12.id="palloncinoC12";
+document.body.appendChild(d12);
+document.getElementById("palloncinoC12").style.position="absolute";
+document.getElementById("palloncinoC12").style.top="0";
+document.getElementById("palloncinoC12").style.left="-500px";
+document.getElementById("palloncinoC12").style.width="90px";
+document.getElementById("palloncinoC12").style.height="180px";
+document.getElementById("palloncinoC12").style.background="transparent";
+document.getElementById("palloncinoC12").style.border="none";
+document.getElementById("palloncinoC12").innerHTML= '<a href="http://iolecal.blogspot.it/2014/03/palloncini-per-il-compleanno-del-blog.html" target="_blank"><img title="&copy; iolecal.blogspot.it" src="https://www.iolecal.it/blog/posts400/post445/10/p_12.png" style="width: 64px; height: 128px; border: none;" /></a>';
+var d18 = document.createElement("div");
+d18.id="palloncinoC18";
+document.body.appendChild(d18);
+document.getElementById("palloncinoC18").style.position="absolute";
+document.getElementById("palloncinoC18").style.top="0";
+document.getElementById("palloncinoC18").style.left="-500px";
+document.getElementById("palloncinoC18").style.width="90px";
+document.getElementById("palloncinoC18").style.height="180px";
+document.getElementById("palloncinoC18").style.background="transparent";
+document.getElementById("palloncinoC18").style.border="none";
+document.getElementById("palloncinoC18").innerHTML= '<a href="http://iolecal.blogspot.it/2014/03/palloncini-per-il-compleanno-del-blog.html" target="_blank"><img title="&copy; iolecal.blogspot.it" src="https://www.iolecal.it/blog/posts400/post445/10/p_18.png" style="width: 64px; height: 128px; border: none;" /></a>';
+var d29 = document.createElement("div");
+d29.id="palloncinoC29";
+document.body.appendChild(d29);
+document.getElementById("palloncinoC29").style.position="absolute";
+document.getElementById("palloncinoC29").style.top="0";
+document.getElementById("palloncinoC29").style.left="-500px";
+document.getElementById("palloncinoC29").style.width="90px";
+document.getElementById("palloncinoC29").style.height="180px";
+document.getElementById("palloncinoC29").style.background="transparent";
+document.getElementById("palloncinoC29").style.border="none";
+document.getElementById("palloncinoC29").innerHTML= '<a href="http://iolecal.blogspot.it/2014/03/palloncini-per-il-compleanno-del-blog.html" target="_blank"><img title="&copy; iolecal.blogspot.it" src="https://www.iolecal.it/blog/posts400/post445/10/p_29.png" style="width: 64px; height: 128px; border: none;" /></a>';
+</script>
