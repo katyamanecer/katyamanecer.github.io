a971b663b86e12b1a1d05837d9b7326eb7325c43
katyamanecer <34971463+katyamanecer@users.noreply.github.com>
2018-11-22 19:38:50 -0600

    Add files via upload

---
+++ fly img.js
@@ -0,0 +1,11 @@
+<DIV ID='flyimage1' STYLE='position:absolute; left: -500px; width:47; height:68;'>
+<a HREF='http://dynamicdrive.com'><IMG BORDER='0' SRC='https://3.bp.blogspot.com/-fcvSXxJ0WPQ/W-T3y4y0TGI/AAAAAAAAZQo/of9Uv7h7HDAcqpNqzZvgJYFSz3ZWmcerQCLcBGAs/s1600/Globo%2Bfiesta.png'/></a>
+</DIV>
+
+<DIV ID='flyimage2' STYLE='position:absolute; left: -500px; width:47; height:68;'>
+<a HREF='http://dynamicdrive.com'><IMG BORDER='0' SRC='https://2.bp.blogspot.com/-geFmuVM5kms/W-S4psvX1uI/AAAAAAAAZQA/C56L2rZH8pYY3xMvP-p87K3tbr7EraGjwCLcBGAs/s1600/palloncini2anni_7.png'/></a>
+</DIV>
+
+<DIV ID='flyimage3' STYLE='position:absolute; left: -500px; width:47; height:68;'>
+<a HREF='http://dynamicdrive.com'><IMG BORDER='0' SRC='https://3.bp.blogspot.com/-87WDs1VpIEk/W-T_Eo_9xlI/AAAAAAAAZRA/qpAj94fL1IAI44KudBOtBopZutFNoSnOQCLcBGAs/s1600/Globo%2BCaritas.png'/></a>
+</DIV>
