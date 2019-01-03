2adfd665fb1ff9bf3a19be634ca9c757a8438034
katyamanecer <34971463+katyamanecer@users.noreply.github.com>
2019-01-03 11:14:49 -0600

    Add files via upload

---
+++ palloncini jose.js
@@ -0,0 +1,37 @@
+var today = new Date();
+document.writeln('<scr' + 'ipt type="text/javascript">');
+document.writeln('// script personalizzato da iolecal.blogspot.it');
+document.writeln('// Floating image script- By Virtual_Max (http://www.geocities.com/siliconvalley/lakes/8620)');
+document.writeln('// Modified by Dynamic Drive for various improvements');
+document.writeln('// Visit Dynamic Drive at http://www.dynamicdrive.com/ for full source code');
+for (var p=0; p<pallonciniMV1.length; p++) {
+document.writeln('var balloonC1' + pallonciniMV1[p] + ';');
+}
+document.writeln('function MVBDay(){');
+for (var p=0; p<pallonciniMV1.length; p++) {
+document.writeln('balloonC1' + pallonciniMV1[p] + '=new Chip("balloonC1' + pallonciniMV1[p] + '",70,150);');
+}
+for (var p=0; p<pallonciniMV1.length; p++) {
+document.writeln('movechip("balloonC1' + pallonciniMV1[p] + '");');
+}
+document.writeln('}');
+document.writeln('if (window.addEventListener)');
+document.writeln('window.addEventListener("load", MVBDay, false)');
+document.writeln('else if (window.attachEvent)');
+document.writeln('window.attachEvent("onload", MVBDay)');
+document.writeln('else if (document.getElementById)');
+document.writeln('window.onload=MVBDay');
+for (var p=0; p<pallonciniMV1.length; p++) {
+document.writeln('var d' + pallonciniMV1[p] + ' = document.createElement("div");');
+document.writeln('d' + pallonciniMV1[p] + '.id="balloonC1' + pallonciniMV1[p] + '";');
+document.writeln('document.body.appendChild(d' + pallonciniMV1[p] + ');');
+document.writeln('document.getElementById("balloonC1' + pallonciniMV1[p] + '").style.position="absolute";');
+document.writeln('document.getElementById("balloonC1' + pallonciniMV1[p] + '").style.top="0";');
+document.writeln('document.getElementById("balloonC1' + pallonciniMV1[p] + '").style.left="-500px";');
+document.writeln('document.getElementById("balloonC1' + pallonciniMV1[p] + '").style.width="90px";');
+document.writeln('document.getElementById("balloonC1' + pallonciniMV1[p] + '").style.height="180px";');
+document.writeln('document.getElementById("balloonC1' + pallonciniMV1[p] + '").style.background="transparent";');
+document.writeln('document.getElementById("balloonC1' + pallonciniMV1[p] + '").style.border="none";');
+document.writeln('document.getElementById("balloonC1' + pallonciniMV1[p] + '").innerHTML= \'<a href="https://shaddaikawaii.blogspot.com/" target="_blank"><img title="&copy; Fiesta de Jose" src="https://4.bp.blogspot.com/-uGJtVAulgT4/XC4kAuW_nXI/AAAAAAAAaCw/NtdFHPpu53IYq4lysX7oTlZE6poe_W9MgCLcBGAs/s1600/palloncini%2Bjose.png" style="width: 70px; height: 150px; border: none;" /></a>\';');
+}
+document.writeln('</scr' + 'ipt>');
